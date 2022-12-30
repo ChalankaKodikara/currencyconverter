c108bf79e7c2b0e29d57fc76b8ca41cb63e2d7e5
ChalankaKodikara <chalankak@sltc.ac.lk>
2022-12-30 22:17:37 +0530

    Initial commit

---
+++ DemoWebServiceClient/src/com/sltc/soa/client/CurrencyConversion.java
@@ -0,0 +1,52 @@
+package com.sltc.soa.client;
+
+import com.sltc.soa.client.stub.CurrencyConversionWs;
+import com.sltc.soa.client.stub.CurrencyConversionWsService;
+
+import java.util.Scanner;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: Thilanka<br/>
+ * Date: 12/20/2022<br/>
+ * Time: 10:31 PM<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class CurrencyConversion {
+    public static void main(String[] args) {
+        CurrencyConversionWsService currencyConversionWsService = new CurrencyConversionWsService();
+        CurrencyConversionWs currencyConversionWs = currencyConversionWsService.getCurrencyConversionWsPort();
+
+        System.out.println( "Please enter the currency" );
+        String sourceCurrency = readInputIntStr();
+
+        System.out.println( "Please enter the target Currency" );
+        String targetCurrency = readInputIntStr();
+
+        System.out.println( "Please input amount to convert" );
+        float amountInSourceCurrency = readInputFloat();
+
+
+
+
+        float result = (float) currencyConversionWs.convert( amountInSourceCurrency, sourceCurrency , targetCurrency); System.out.print( "Your result is = ");
+        System.out.println(result);
+
+
+
+
+
+    }
+    private static String readInputIntStr()
+    {
+        Scanner scan = new Scanner( System.in );
+        String inputStr = scan.next();
+        return inputStr;
+    }
+    private static float readInputFloat()
+    {
+        Scanner scan = new Scanner( System.in );
+        float inputfloat = scan.nextFloat();
+        return inputfloat;
+        }
+}
